297cd55d736958a043e504f947de95b82af3b077
kazma <kazma.chihaya@arrow-payment.com>
2022-04-23 13:52:19 +0900

    変数のインライン化

---
+++ 変数のインライン化/例_after.js
@@ -0,0 +1,5 @@
+// basePrice変数に変数のインライン化を施しなさい
+
+function example(anOrder) {
+  return anOrder.price > 1000;
+}
---
+++ 変数のインライン化/例_before.js
@@ -0,0 +1,6 @@
+// basePrice変数に変数のインライン化を施しなさい
+
+function example(anOrder) {
+  let basePrice = anOrder.basePrice;
+  return (basePrice > 1000);
+}
